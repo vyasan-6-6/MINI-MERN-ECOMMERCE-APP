23ee66812afd0217e23e08a0883272f56ee94c35
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-01-21 12:36:45 +0530

    Prepare backend for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -8,13 +8,16 @@ const productRoutes = require("./routes/productRoutes");
   
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
  
 connectDB();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:process.env.CLIENT_URL,
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
