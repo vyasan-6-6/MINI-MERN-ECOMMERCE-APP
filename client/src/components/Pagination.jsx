38354de19bcf5a1459dd6d1f86a7ec1eff9bd19d
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-01-21 11:41:58 +0530

    fixed the pagination state

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+
 import { useProductContext } from '../contextApi/useProductContext'
 
 const Pagination = () => {
-    const {state} = useProductContext();
+    const {state,dispatch} = useProductContext();
   return (
     <div className='flex justify-center gap-2 mt-8'> 
         <button 
         disabled={state.page ===1}
-        onClick={()=>setPage(state.page-1)}
+        onClick={()=>dispatch({type:"SET_PAGE",payload:state.page-1})}
         className='px-4 py-2 border rounded disabled:opacity-50'
         >
             prev
         </button>
         {[...Array(state.pages).keys()].map((x)=>(
          <button key={x+1}
-         onClick={()=> setPage(x+1)}
+          onClick={()=>dispatch({type:"SET_PAGE",payload:x+1})}
          className={`px-4 py-2 border rounded ${state.page === x+1 ?"bg-blue-500 text-white": "" }`}
          >
             {x+1}
          </button>
         ))}
         <button disabled={state.page === state.pages}
-        onClick={()=>setPage(state.page+1)}
+         onClick={()=>dispatch({type:"SET_PAGE",payload:state.page+1})}
         className='px-4 py-2 border rounded disabled:opacity-50'
         >
             Next
